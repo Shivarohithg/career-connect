722b787246456cf8aee1a54edbd4a9916dcd4f69
shiva <shivarohithganta@gmail.com>
2026-07-15 23:14:15 +0530

    Configure React Router and create navigation layout

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+
+            <h2 className={styles.logo}>
+                CareerConnect
+            </h2>
+
+            <ul className={styles.navLinks}>
+
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+
+            </ul>
+
+        </nav>
+    );
+}
+
+export default Navbar;
