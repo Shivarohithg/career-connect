dbabc5ef52ee8668660d9b51adc28f362216e7b6
shiva <shivarohithganta@gmail.com>
2026-09-09 14:15:29 +0530

    Day 54: Complete authentication and protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
+
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
